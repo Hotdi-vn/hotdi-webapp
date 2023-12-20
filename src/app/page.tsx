6c6742a3215215314fc88883562dbe54e9a89d27
Nguyễn Thị Phương Thảo <thao031103@gmail.com>
2023-12-20 23:32:41 +0000

    Hotdi blog component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Banner from '@/components/banner/Banner';
-import CategoryList from '@/components/category-list/CategoryList'
+import CategoryList from '@/components/category-list/CategoryList';
 import { NavBar, SearchBar } from '@/components/common/antd_mobile_client_wrapper';
 import FarmExplorerComponent from '@/components/farm-explorer/FarmExplorer';
-import Icon from '@/components/common/icon_component'
+import Icon from '@/components/common/icon_component';
+import HotdiBlog from '@/components/hotdi-blog/HotdiBlog';
 
 export default function Home() {
   const navBar =
@@ -15,6 +16,7 @@ export default function Home() {
       <div className='top'>{navBar}</div>
       <Banner />
       <CategoryList />
+      <HotdiBlog />
       <FarmExplorerComponent />
     </div>
   )
