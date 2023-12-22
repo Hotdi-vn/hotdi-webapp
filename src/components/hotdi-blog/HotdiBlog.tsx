97e4fb4d33bfb8141cf85c9fb7dcda89dd544b49
Thao Nguyen <71063988+immattao@users.noreply.github.com>
2023-12-22 08:13:58 -0500

    Change avatars of the hotdi blogs

--- src/components/hotdi-blog/HotdiBlog.tsx
+++ src/components/hotdi-blog/HotdiBlog.tsx
@@ -5,19 +5,19 @@ import styles from './HotdiBlog.module.css'
 export default function HotdiBlog() {
     const blogs: BlogItem[] = [
         new BlogItem(
-            '/hotdi-blog/nali-avatar.svg',
+            '/hotdi-blog/hieu-sinh-avatar.svg',
             'Hiếu Sinh',
             new Date(),
             'Hotdi.vn ra đời với sứ mệnh tạo ra một sàn thương mại điện tử mua bán nông sản sạch, nơi mọi giao dịch đều là sự đóng góp cho cuộc sống toàn diện và môi trường bền'
         ),
         new BlogItem(
-            '/hotdi-blog/vuon-rung-cua-la-avatar.svg',
+            '/hotdi-blog/another-avatar.svg',
             'Hiếu Sinh',
             new Date(),
             'Một trong những điểm du lịch quá nổi tiếng ở tỉnh cực Bắc Thái Lan, Singha Park mang trong mình một xứ mệnh là làm thao thức bao con tim của du khách. Mình đã đi rất nhiều n'
         ),
         new BlogItem(
-            '/hotdi-blog/nali-avatar.svg',
+            '/hotdi-blog/hieu-sinh-avatar.svg',
             'Hiếu Sinh',
             new Date(),
             'Hotdi.vn ra đời với sứ mệnh tạo ra một sàn thương mại điện tử mua bán nông sản sạch, nơi mọi giao dịch đều là sự đóng góp cho cuộc sống toàn diện và môi trường bền'
