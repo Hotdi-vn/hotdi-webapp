5e65527b1281b2d5485a8ea11f9e0012efc2b9c1
Thao Nguyen <thao031103@gmail.com>
2024-02-08 23:00:11 -0500

    Fetch blog data using server-side fetching

--- src/components/hotdi-blog/HotdiBlog.tsx
+++ src/components/hotdi-blog/HotdiBlog.tsx
@@ -1,63 +1,55 @@
-'use client'
+
 import BlogItemComponent, { BlogItem } from "./blog-item/BlogItemComponent";
 import Link from "next/link";
 import styles from './HotdiBlog.module.css'
 import { RightOutline } from '@/components/common/antd_mobile_client_wrapper';
-import { useSWRandGenerateData } from '@/utils/client-side-fetching';
+import { getNoCache } from '@/utils/server-side-fetching'
 
-export default function HotdiBlog() {
-    // const blogs: BlogItem[] = [
-    //     new BlogItem(
-    //         '/hotdi-blog/hieu-sinh-avatar.svg',
-    //         'Hiếu Sinh',
-    //         new Date(),
-    //         'Hotdi.vn ra đời với sứ mệnh tạo ra một sàn thương mại điện tử mua bán nông sản sạch, nơi mọi giao dịch đều là sự đóng góp cho cuộc sống toàn diện và môi trường bền'
-    //     ),
-    //     new BlogItem(
-    //         '/hotdi-blog/another-avatar.svg',
-    //         'Hiếu Sinh',
-    //         new Date(),
-    //         'Một trong những điểm du lịch quá nổi tiếng ở tỉnh cực Bắc Thái Lan, Singha Park mang trong mình một xứ mệnh là làm thao thức bao con tim của du khách. Mình đã đi rất nhiều n'
-    //     ),
-    //     new BlogItem(
-    //         '/hotdi-blog/hieu-sinh-avatar.svg',
-    //         'Hiếu Sinh',
-    //         new Date(),
-    //         'Hotdi.vn ra đời với sứ mệnh tạo ra một sàn thương mại điện tử mua bán nông sản sạch, nơi mọi giao dịch đều là sự đóng góp cho cuộc sống toàn diện và môi trường bền'
-    //     )
-    // ]
-    // return (
-    //     <div className={styles.blogComponent}>
-    //         <div className={styles.heading}>
-    //             <h1 className={styles.h1}>{'Hotdi\'s Blog'}</h1>
-    //             <Link href='https://www.facebook.com/groups/881860672889799' className={styles.a}>
-    //                 Xem thêm <RightOutline />
-    //             </Link>
-    //         </div>
-    //         <div className={styles.blogs}>
-    //             {
-    //                 blogs.map((blog, index) => <BlogItemComponent key={index} blogItem={blog} />)
-    //             }
-    //         </div>
-    //     </div>
-    // );
-    
-    const dataGeneration = (blogs: BlogItem[]) => {
-        return (
-            <div className={styles.blogComponent}>
-                <div className={styles.heading}>
-                    <h1 className={styles.h1}>{'Hotdi\'s Blog'}</h1>
-                    <Link href='/hotdi-blog' className={styles.a}>
-                        Xem thêm <RightOutline />
-                    </Link>
-                </div>
-                <div className={styles.blogs}>
-                    {
-                        blogs.map((blog, index) => <BlogItemComponent key={index} blogItem={blog} />)
-                    }
-                </div>
-            </div>
-        )
+export default async function HotdiBlog() {
+    type blogData = {
+        _id: string;
+        title: string;
+        content: string;
+        imageUrls: string[];
+        createdBy: string;
+        createdAt: number;
+        updatedAt: number;
     };
-    return useSWRandGenerateData('/blog/v1/posts', dataGeneration);
+
+    type blogDataResponse = {
+        data: blogData[];
+        count: number;
+    }
+
+    let blogs: BlogItem[] = [];
+
+    await getNoCache<blogData[]>('/blog/v1/posts').then((returnedObject) => {
+        blogs = returnedObject.data.map((dataItem: blogData) => {
+            return (
+                //TODO: get avatar and name from login info
+                new BlogItem(
+                    '/hotdi-blog/hieu-sinh-avatar.svg',
+                    dataItem.createdBy,
+                    new Date(dataItem.updatedAt),
+                    dataItem.title.concat('\n', dataItem.content)
+                )
+            );
+        });
+    });
+
+    return (
+        <div className={styles.blogComponent}>
+            <div className={styles.heading}>
+                <h1 className={styles.h1}>{'Hotdi\'s Blog'}</h1>
+                <Link href='#' className={styles.a}>
+                    Xem thêm <RightOutline />
+                </Link>
+            </div>
+            <div className={styles.blogs}>
+                {
+                    blogs.map((blog, index) => <BlogItemComponent key={index} blogItem={blog} />)
+                }
+            </div>
+        </div>
+    );
 }
