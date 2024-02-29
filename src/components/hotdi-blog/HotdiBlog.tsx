876761dc51d173b3fa73b8fc762fe1ba8be90339
Thao Nguyen <thao031103@gmail.com>
2024-02-29 15:12:33 -0500

    Get blog author profile from auth API

--- src/components/hotdi-blog/HotdiBlog.tsx
+++ src/components/hotdi-blog/HotdiBlog.tsx
@@ -1,11 +1,13 @@
 
 import BlogItemComponent, { BlogItem } from "./blog-item/BlogItemComponent";
 import Link from "next/link";
-import styles from './HotdiBlog.module.css'
+import styles from './HotdiBlog.module.css';
 import { RightOutline } from '@/components/common/antd_mobile_client_wrapper';
-import { getNoCache } from '@/utils/server-side-fetching'
+import { getNoCache, get } from '@/utils/server-side-fetching';
+import { UserProfile } from "@/libs/session-options";
 
 export default async function HotdiBlog() {
+
     type blogData = {
         _id: string;
         title: string;
@@ -14,22 +16,38 @@ export default async function HotdiBlog() {
         createdBy: string;
         createdAt: number;
         updatedAt: number;
+        authorName: string;
+        avatarURL: string;
     };
 
-    type blogDataResponse = {
-        data: blogData[];
-        count: number;
+    type userProfile = {
+        _id: string,
+        name: string,
+        picture: string;
+        createdAt: number;
     }
 
     let blogs: BlogItem[] = [];
 
     await getNoCache<blogData[]>('/blog/v1/posts').then((returnedObject) => {
-        blogs = returnedObject.data.map((dataItem: blogData) => {
+        //get the blogs in the data field of the object
+        let blogDataList = returnedObject.data;
+        console.log(blogDataList);
+
+        //get avatar and name of author from facebook
+        blogDataList.forEach(async (dataItem) => {
+            await get<userProfile>('/auth/v1/users/' + dataItem.createdBy, 30).then((user) => {
+                (dataItem as any).authorName = user.data.name;
+                (dataItem as any).avatarURL = user.data.picture;
+            })
+        })
+
+        //map data to BlogItem array
+        blogs = blogDataList.map((dataItem: blogData) => {
             return (
-                //TODO: get avatar and name from login info
                 new BlogItem(
-                    '/hotdi-blog/hieu-sinh-avatar.svg',
-                    dataItem.createdBy,
+                    dataItem.avatarURL,
+                    dataItem.authorName,
                     new Date(dataItem.updatedAt),
                     dataItem.title.concat('\n', dataItem.content)
                 )
@@ -40,7 +58,7 @@ export default async function HotdiBlog() {
     return (
         <div className={styles.blogComponent}>
             <div className={styles.heading}>
-                <h1 className={styles.h1}>{'Hotdi\'s Blog'}</h1>
+                <h1 className={styles.h1}>{"Hotdi's Blog"}</h1>
                 <Link href='#' className={styles.a}>
                     Xem thêm <RightOutline />
                 </Link>
