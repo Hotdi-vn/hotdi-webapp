4f4e4e638ee6a1e9eb05c46b9c1cc29d59eb4c7d
Thao <thao031103@gmail.com>
2024-04-24 15:18:35 -0400

    Integrate banners API

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,19 +1,31 @@
 'use client'
 import { Swiper } from 'antd-mobile';
 import Image from 'next/image';
+import Link from 'next/link'
 import styles from './Banner.module.css'
+import { useSWRandGenerateData } from '@/utils/client-side-fetching'; 
 
 export default function Banner() {
-    const images = [
-        { imageSrc: '/banner1.png', imageAlt: 'banner-1' },
-    ];
-
-    const bannerItems = images.map(({ imageSrc, imageAlt }, index) => (
-        <Swiper.Item key={index} className={styles.bannerItem}>
-            <Image src={imageSrc} alt={imageAlt} height={130} width={440} priority={true}></Image>
-        </Swiper.Item>
-    ));
-    return (
-        <Swiper className={styles.banner}>{bannerItems}</Swiper>
+    type Banner = {
+        _id: string,
+        name: string,
+        imageUrl: string,
+        link: string,
+        createdBy: string,
+        createdAt: number,
+        updatedAt: number
+    }
+    
+    const generateDataFunc = (images: Banner[]) => (
+        <Swiper className={styles.banner}>{
+            images.map(({ name, imageUrl, link }, index) => (
+                <Swiper.Item key={index} className={styles.bannerItem}>
+                    <Link href={link}>
+                        <Image src={imageUrl} alt={name} height={130} width={440} priority={true}></Image>
+                    </Link>
+                </Swiper.Item>
+            ))
+        }</Swiper>
     );
+    return useSWRandGenerateData<Banner[]>('/market/v1/banners', generateDataFunc);
 }
