2f5bc349143b77f057c5ce3b62483dff0ad2d01d
Huy <xuanhuypham1501@gmail.com>
2024-05-30 00:07:44 +0700

    feat: add preview iamge

--- src/components/product-details/carousel-product.tsx
+++ src/components/product-details/carousel-product.tsx
@@ -5,10 +5,13 @@ const CarouselProduct = (prop: any) => {
     const { imageUrls } = prop
     const [images, setImages] = useState(imageUrls)
     const [activeImg, setActiveImage] = useState(imageUrls[0])
+    const [openModal, SetOpenModal] = useState(false)
     return (
         <div className='flex flex-col justify-between lg:flex-row bg-white'>
             <div className='flex flex-col gap-6 block'>
-                <img src={activeImg} alt="" className='w-full h-full aspect-square object-cover rounded-xl'/>
+                <img src={activeImg} alt="" className='w-full h-full aspect-square object-cover rounded-xl' onClick={() => {
+                    SetOpenModal(true)
+                }}/>
                 <div className='flex flex-row h-24 overflow-scroll flex-nowrap'>
                     {images.map((image: string, index: number) => {
                         return (
@@ -16,6 +19,18 @@ const CarouselProduct = (prop: any) => {
                         )
                     })}
                 </div>
+            </div>
+            <div id="modal"
+                className={openModal ? "" : "hidden"}>
+                    <div className="fixed top-0 left-0 z-80 w-screen h-screen bg-black/70 flex justify-center items-center">
+                    <a className="fixed z-90 top-14 right-2
+                        text-white text-5xl font-bold" 
+                href="javascript:void(0)"
+                onClick={() => {SetOpenModal(false)}}>
+                    ×
+                </a>
+                <img id="modal-img" src={activeImg} className="max-w-[400px] max-h-[600px] object-cover"/>
+                    </div>
             </div>
         </div>
     )
