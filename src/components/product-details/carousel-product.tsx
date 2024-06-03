00b7be06d4463a7fcab08e8f464521f2e4722da7
Huy <xuanhuypham1501@gmail.com>
2024-06-04 00:38:15 +0700

    feat: update to antd component

--- src/components/product-details/carousel-product.tsx
+++ src/components/product-details/carousel-product.tsx
@@ -1,37 +1,47 @@
 'use client'
 import React, { useState } from 'react'
+import { ImageViewer } from 'antd-mobile'
+import Image from 'next/image'
 
 const CarouselProduct = (prop: any) => {
     const { imageUrls } = prop
     const [images, setImages] = useState(imageUrls)
     const [activeImg, setActiveImage] = useState(imageUrls[0])
-    const [openModal, SetOpenModal] = useState(false)
+    const [visible, setVisible] = useState(false)
     return (
         <div className='flex flex-col justify-between lg:flex-row bg-white'>
             <div className='flex flex-col gap-6 block'>
-                <img src={activeImg} alt="" className='w-full h-full aspect-square object-cover rounded-xl' onClick={() => {
-                    SetOpenModal(true)
-                }}/>
+                <Image
+                    width={400}
+                    height={400}
+                    src={activeImg} 
+                    alt="" 
+                    className='w-full h-full aspect-square object-cover rounded-xl' 
+                    onClick={() => {
+                        setVisible(true)
+                    }}/>
                 <div className='flex flex-row h-24 overflow-scroll flex-nowrap'>
                     {images.map((image: string, index: number) => {
                         return (
-                            <img src={image} key={index} alt="" className='w-24 h-24 rounded-md cursor-pointer border-solid border-2 border-black mx-3 float-left' onClick={() => setActiveImage(image)}/>
+                            <Image 
+                            width={96}
+                            height={96}
+                            src={image} key={index} alt="" className='w-24 h-24 rounded-md cursor-pointer border-solid border-2 border-black mx-3 float-left' onClick={() => setActiveImage(image)}/>
                         )
                     })}
                 </div>
             </div>
-            <div id="modal"
-                className={openModal ? "" : "hidden"}>
-                    <div className="fixed top-0 left-0 z-80 w-screen h-screen bg-black/70 flex justify-center items-center">
-                    <a className="fixed z-90 top-14 right-2
-                        text-white text-5xl font-bold" 
-                href="javascript:void(0)"
-                onClick={() => {SetOpenModal(false)}}>
-                    ×
-                </a>
-                <img id="modal-img" src={activeImg} className="max-w-[400px] max-h-[600px] object-cover"/>
-                    </div>
-            </div>
+            <ImageViewer
+                classNames={{
+                mask: 'customize-mask',
+                body: 'customize-body',
+                }}
+                image={activeImg}
+                visible={visible}
+                onClose={() => {
+                setVisible(false)
+                }}
+            />
         </div>
     )
 }
